7b6c3e32fe6f3bd8c1a4e7f2f18c92613420ba5a
Naman <namanjain2050@gmail.com>
2023-11-13 23:19:44 +0530

    added shadcn-ui button

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <p className="text-6xl text-green-500">Hello AI SaaS</p>
+    <Button variant="destructive" size="lg">Click me</Button>
   )
 }

5bc3ef01d82d0e01ec42331602089be7d383ddce
Naman <namanjain2050@gmail.com>
2023-11-13 23:39:11 +0530

    created route groups

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const DashboardPage = () => {
+  return <p>Dashboard Page (Protected)</p>;
+};
+
+export default DashboardPage;
